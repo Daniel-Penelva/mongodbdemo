2fbacc80a192f6e4da143ea064349c150f403fa3
Daniel Penelva <d4n.andrade@gmail.com>
2023-11-01 01:13:10 -0300

    Alterado application.yml e docker-compose + utilizando CommandLineRunner e Builder

--- src/main/java/com/daniel/coding/mongodbdemo/MongodbDemoApplication.java
+++ src/main/java/com/daniel/coding/mongodbdemo/MongodbDemoApplication.java
@@ -1,13 +1,31 @@
 package com.daniel.coding.mongodbdemo;
 
+import com.daniel.coding.mongodbdemo.product.Product;
+import com.daniel.coding.mongodbdemo.product.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MongodbDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbDemoApplication.class, args);
+
+
 	}
 
+	/* O Script abaixo significa que eu quero executar algum código na inicialização do aplicativo*/
+	@Bean
+	public CommandLineRunner commandLineRunner(ProductRepository productRepository){
+		return args -> {
+			var product = Product.builder()
+					.name("iPhone")
+					.description("Smart Phone")
+					.build();
+
+			productRepository.insert(product);
+		};
+	}
 }
